79e868df80bc9128077c91a6034fa5f0122ca18a
Gwebs <theelithe555@gmail.com>
2019-01-04 19:55:15 +0000

    little correction

--- bot.py
+++ bot.py
@@ -58,24 +58,24 @@ async def play(ctx):
 
 
 
-@client.event
-async def on_message(message):
-    author = message.author
-    content = message.content
-    print(f"{author} said : {content}")
-    await client.process_commands(message)
+    @client.event
+    async def on_message(message):
+        author = message.author
+        content = message.content
+        print(f"{author} said : {content}")
+        await client.process_commands(message)
 
-@client.command()
-async def ping():
-    await client.say('Pong!')
-@client.command(pass_context=True)
-async def say(ctx, *args):
-    await client.delete_message(ctx.message)
-    output = ''
-    for word in args:
-        output += word
-        output += ' '
-    await client.say(output) 
+    @client.command()
+    async def ping():
+        await client.say('Pong!')
+    @client.command(pass_context=True)
+    async def say(ctx, *args):
+        await client.delete_message(ctx.message)
+        output = ''
+        for word in args:
+            output += word
+            output += ' '
+        await client.say(output) 
 
 
-client.run(token)
+    client.run(token)
